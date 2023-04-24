ee6bf4f4962b5f9385ba086825800b8a011f2a2f
JoaoGeraldo15 <joaogeraldo40@gmail.com>
2023-04-23 22:04:27 -0300

    Refacotr: ajuste interpretador que seria executado para single game. Mudança para interpretador do venv

--- src/service/at_task_service.py
+++ src/service/at_task_service.py
@@ -5,5 +5,5 @@ from injectable import injectable
 @injectable
 class AtTaskService():
     def do(self, script_path:str, param1: str, param2:str, date: str):
-        cmd = f"echo 'python3.10 {script_path} {param1} {param2}' | at {date}"
+        cmd = f"echo 'arbitrage-betting-knapsack-problem/venv/bin/python3 {script_path} {param1} {param2}' | at {date}"
         os.system(cmd)
