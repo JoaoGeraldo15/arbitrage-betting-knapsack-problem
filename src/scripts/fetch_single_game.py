bf863ecaf1429d89020342e7311642dc793a0ccc
joaogeraldo15 <joaogeraldo40@gmail.com>
2023-08-20 01:19:35 -0300

    Feat: Trocando chave da api caso esgote os créditos na busca de jogos únicos.

--- src/scripts/fetch_single_game.py
+++ src/scripts/fetch_single_game.py
@@ -1,4 +1,10 @@
+import os
+
 import sys
+import time
+
+from requests import Response
+
 sys.path.append('/root/arbitrage-betting-knapsack-problem')
 import json
 import sys
@@ -8,23 +14,29 @@ import requests
 from src.config.config import API_KEY
 from src.schema import GameBase
 from src.service.game_service import GameService
-from src.util import checkout_root_path
-
+from src.util import checkout_root_path, replace_api_key
 
 if __name__ == '__main__':
-    checkout_root_path()
+    os.chdir('/root/arbitrage-betting-knapsack-problem')
     game_id = sys.argv[1]
     sport = sys.argv[2]
 
     params = {
-        'apiKey': API_KEY,
+        'apiKey': API_KEY.split(',')[0],
         'region': 'eu',
         'markets': 'totals,btts,draw_no_bet',
         'bookmakers': 'betfair,betsson,matchbook,pinnacle,williamhill,sport888,onexbet,betonlineag,unibet_eu,nordicbet'
     }
 
     URL_ODDS = f'https://api.the-odds-api.com/v4/sports/{sport}/events/{game_id}/odds'
-    odds_response = requests.get(url=URL_ODDS, params=params)
+    odds_response = Response()
+    odds_response.status_code = 1
+    while odds_response.status_code != 200:
+        odds_response = requests.get(url=URL_ODDS, params=params)
+        if odds_response.status_code != 200 and int(odds_response.headers['X-Requests-Remaining']) < 30:
+            replace_api_key(params['apiKey'])
+            time.sleep(1.5)
+            params['apiKey'] = API_KEY.split(',')[0]
 
     log = f"[Requests-Used]: {odds_response.headers['X-Requests-Used']} \n[Requests-Remaining]: {odds_response.headers['X-Requests-Remaining']} \n[Date]: {odds_response.headers['Date']}"
     with open('log/log_jogos', 'w') as f:
