4c89cd8183dc8d291a980249b4166ce1a5c960ce
joaogeraldo15 <joaogeraldo40@gmail.com>
2023-08-20 01:33:02 -0300

    Fix: Removendo import pandas sem uso

--- src/repository/outcome_repository.py
+++ src/repository/outcome_repository.py
@@ -1,5 +1,4 @@
 from typing import List
-import pandas as pd
 from injectable import injectable
 
 from src.config.connection import DBConnection
@@ -19,23 +18,3 @@ class OutcomeRepository:
             return db.session.query(Outcome).filter(
                 Outcome.point == point).all()
 
-
-if __name__ == '__main__':
-    repository = OutcomeRepository()
-    result = repository.find_all_points()
-    outcomes = repository.find_all(2.5)
-
-    # Supondo que você tenha uma lista de objetos chamada lista_objetos
-    lista_dicionarios = [
-        {
-            'id':obj.id,
-             'name': obj.name,
-             'price': obj.price,
-             'point': obj.point,
-             'update_time': obj.update_time
-        }
-        for obj in outcomes]
-
-
-    records = pd.DataFrame.from_records(lista_dicionarios)
-    print(records.head())
