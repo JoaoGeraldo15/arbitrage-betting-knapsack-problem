d75c9c01f8b82a046c126e622554d393c9dd00d3
joaogeraldo15 <joaogeraldo40@gmail.com>
2023-08-23 23:03:34 -0300

    Fix: log em fetch leagues

--- src/scripts/fetch_leagues.py
+++ src/scripts/fetch_leagues.py
@@ -14,15 +14,17 @@ if __name__ == '__main__':
     response.status_code = 1
     amount_api_key = len(str(API_KEY).split(','))
     index_api_key = int(LAST_API_KEY_USED)
+    os.system(f"echo iniciando  >> ../fetch_leagues.txt")
     while response.status_code != 200 and index_api_key != amount_api_key:
         key = f"{API_KEY.split(',')[index_api_key]}"
         URL = f'https://api.the-odds-api.com/v4/sports?apiKey={key}'
         response = requests.get(URL)
         index_api_key += 1
-
+        os.system(f"echo key: '{key}' amount_api_key: '{amount_api_key}' >> ../fetch_leagues.txt")
     if response.status_code == 200:
         data = response.json()
         leagues = [league['key'] for league in data if 'soccer_' in league['key']]
+        os.system(f"echo leagues: '{leagues}' >> ../fetch_leagues.txt")
 
         if len(leagues):
             game_service = GameService()
