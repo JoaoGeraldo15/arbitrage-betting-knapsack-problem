9181562937e1df22149a11c193e1ede998521991
joaogeraldo15 <joaogeraldo40@gmail.com>
2023-09-26 21:51:47 -0300

    Feat: Adição de crossovers e elaboração de main

--- src/genetic/arbitrage.py
+++ src/genetic/arbitrage.py
@@ -1,10 +1,11 @@
 import math
-from random import random
+import random
+from typing import List
 
 
 class Arbitrage:
     # def __init__(self, game_id, outcome_id_OVER, outcome_id_UNDER, bookmaker_key_OVER,
-                 # bookmaker_key_UNDER, odd_OVER, odd_UNDER, last_update_OVER, last_update_UNDER, profit):
+    # bookmaker_key_UNDER, odd_OVER, odd_UNDER, last_update_OVER, last_update_UNDER, profit):
     def __init__(self, game_id, outcome_id_OVER, outcome_id_UNDER, bookmaker_key_OVER,
                  bookmaker_key_UNDER, odd_OVER, odd_UNDER, profit):
         self.game_id = game_id
@@ -30,38 +31,46 @@ class Arbitrage:
 
 
 class Individual:
-    def __init__(self, arbitrages, budget, geracao=0):
+    def __init__(self, arbitrages, budget, geracao=0, init_population=False):
         self.arbitrages = arbitrages
         self.profits = [i.profit for i in arbitrages]
         self.budget = budget
         self.geracao = geracao
         self.fitness = []
+        self.fitness_normalized = []
         self.fitness_max_profit = None
         self.fitness_diversification = None
-        self.chromosome = []
+        self.chromosome: list[tuple[int, float]] = []
+        self.dominated_by = []  # Lista de soluções que dominam esta solução
+        self.dominated_solutions = []  # Lista de soluções que esta solução domina
+        self.dominated_by_count = 0  # Por quantas soluções esta solução é dominada
+        self.rank = None  # Rank da solução nas frentes de Pareto
+        self.crowding_distance = None  # Distância de lotação da solução
 
+        if init_population:
+            self.initialize_individual(arbitrages)
+
+    def initialize_individual(self, arbitrages):
         total_profit = 0
         count_profit = 0
-
         for i in range(len(arbitrages)):
-            valor = round(random(), 2)
+            valor = round(random.random(), 2)
             if valor < 0.5:
                 self.chromosome.append((0, valor))
             else:
                 self.chromosome.append((1, valor))
                 total_profit += self.profits[i]
                 count_profit += 1
-
-        self.normalize()
+        self.normalize_budge_allocation()
         self.evaluate_fitness()
 
-    def normalize(self):
+    def normalize_budge_allocation(self):
         total = sum([chromosome[1] for chromosome in self.chromosome if chromosome[0] == 1])
 
         for index in range(len(self.chromosome)):
             chromosome = self.chromosome[index]
             if chromosome[0] == 1:
-                new_value_normalized = round(chromosome[1]/total, 2)
+                new_value_normalized = round(chromosome[1] / total, 2)
                 self.chromosome[index] = (1, new_value_normalized)
 
     def objective_function_max_profit(self):
@@ -90,6 +99,15 @@ class Individual:
         self.objective_function_max_profit()
         self.objective_function_diversification()
 
+    def normalize_fitness(self, highest_fitness):
+        num_objectives = len(self.fitness)
+        for obj_index in range(num_objectives):
+            fit = highest_fitness[obj_index]
+            self.fitness_normalized.append(round(self.fitness[obj_index] / fit, 4))
+
+    def print_chromossome(self):
+        print(f'{self.chromosome} --> {self.fitness} --> {self.fitness_normalized}')
+
 
 class Population:
     def __init__(self, num_individuals, arbitrages, budget):
@@ -102,21 +120,105 @@ class Population:
             budget (float): Orçamento disponível para alocação nas apostas.
         """
         self.num_individuals = num_individuals
-        self.solutions = []  # Lista de todas as soluções
+        self.solutions: List[Individual] = []  # Lista de todas as soluções
+        self.step_solutions: List[Individual] = []  # Lista auxiliar que será usada para construir a geração de filhos
+        self.high_fitness_evaluted = [0, 0]
         self.pareto_fronts = []  # Lista de listas representando as frentes de Pareto
+        self.solutions_history: List = []
 
         for _ in range(num_individuals):
-            individual = Individual(arbitrages, budget)
-            self.solutions.append(Solution(individual.fitness_max_profit, individual.fitness_diversification, [individual.fitness_max_profit, individual.fitness_diversification]))
+            individual = Individual(arbitrages, budget, init_population=True)
+            self.solutions.append(individual)
 
-        self.initialize_pareto_fronts()
-        self.assign_rank()
-        self.calculate_crowding_distance()
+        self.__normalize_fitness_population(self.solutions)
+        self.__initialize_pareto_fronts()
+        self.__assign_rank()
+        self.__calculate_crowding_distance()
 
-    def initialize_pareto_fronts(self):
+    def main(self, t=10):
+        # Selecionar os pais e realizar cruzamento, mutação até ter uma população 2N
+        while t > 0:
+            print(f'Iteração: [{5-t}]')
+            self.step_solutions = []
+            while len(self.step_solutions) < self.num_individuals:  # Gerando os filhos
+                self.__uniform_crossover(self.__tournament_selection(), self.__tournament_selection())
+            self.__normalize_fitness_population(self.step_solutions)
+            self.solutions.extend(self.step_solutions[: self.num_individuals])
+            self.__initialize_pareto_fronts()
+            self.__assign_rank()
+            self.__calculate_crowding_distance()
+            self.__select_survivors()
+            self.__commit_history_solutions()
+            t -= 1
+
+    def __tournament_selection(self) -> Individual:
+        tournament = random.sample(self.solutions, 2)
+        tournament.sort(key=lambda x: (x.rank, -x.crowding_distance))
+        return tournament[0]
+
+    def __uniform_crossover(self, parent_1: Individual, parent_2: Individual):
+        child_1 = Individual(parent_1.arbitrages, parent_1.budget)
+        child_2 = Individual(parent_1.arbitrages, parent_1.budget)
+
+        for gene_parent_1, gene_parent_2 in zip(parent_1.chromosome, parent_2.chromosome):
+            if random.random() < 0.5:  # 50% de chance de herdar do parent_1
+                child_1.chromosome.append(gene_parent_1)
+                child_2.chromosome.append(gene_parent_2)
+            else:  # 50% de chance de herdar do parent_2
+                child_1.chromosome.append(gene_parent_2)
+                child_2.chromosome.append(gene_parent_1)
+
+        child_1.normalize_budge_allocation()
+        child_2.normalize_budge_allocation()
+        child_1.evaluate_fitness()
+        child_2.evaluate_fitness()
+        self.step_solutions.extend([child_1, child_2])
+
+    def __uniform_crossover_one_individual(self, parent_1: Individual, parent_2: Individual):
+        child = Individual(parent_1.arbitrages, parent_1.budget)
+
+        for gene_parent_1, gene_parent_2 in zip(parent_1.chromosome, parent_2.chromosome):
+            if random.random() < 0.5:  # 50% de chance de herdar do parent_1
+                child.chromosome.append(gene_parent_1)
+            else:  # 50% de chance de herdar do parent_2
+                child.chromosome.append(gene_parent_2)
+
+        child.normalize_budge_allocation()
+        child.evaluate_fitness()
+        self.step_solutions.append(child)
+
+    def __one_point_crossover(self, parent_1: Individual, parent_2: Individual):
+        cut_point_crossover = random.randrange(len(parent_1.chromosome))
+
+        child_1_chromosome = parent_1.chromosome[:cut_point_crossover] + parent_2.chromosome[cut_point_crossover:]
+        child_2_chromosome = parent_2.chromosome[:cut_point_crossover] + parent_1.chromosome[cut_point_crossover:]
+
+        child_1 = Individual(parent_1.arbitrages, parent_1.budget)
+        child_2 = Individual(parent_1.arbitrages, parent_1.budget)
+
+        child_1.chromosome = child_1_chromosome
+        child_2.chromosome = child_2_chromosome
+        child_1.normalize_budge_allocation()
+        child_2.normalize_budge_allocation()
+        child_1.evaluate_fitness()
+        child_2.evaluate_fitness()
+        self.step_solutions.extend([child_1, child_2])
+
+    def __normalize_fitness_population(self, solutions: List[Individual]):
+        num_objectives = len(solutions[0].fitness)
+        self.high_fitness_evaluted = [0 for _ in range(num_objectives)]
+
+        for obj_index in range(num_objectives):
+            for individual in solutions:
+                if individual.fitness[obj_index] > self.high_fitness_evaluted[obj_index]:
+                    self.high_fitness_evaluted[obj_index] = individual.fitness[obj_index]
+
+        for solution in solutions:
+            solution.normalize_fitness(self.high_fitness_evaluted)
+
+    def __initialize_pareto_fronts(self):
         """
             Inicializa as frentes de Pareto da população e identifica as soluções não dominadas na primeira frente.
-
        """
         # Inicialize as estruturas para armazenar as frentes de Pareto
         for solution in self.solutions:
@@ -134,7 +236,7 @@ class Population:
                         solution1.dominated_solutions.append(solution2)
 
                     elif (solution2.fitness_max_profit >= solution1.fitness_max_profit
-                            and solution2.fitness_diversification >= solution1.fitness_diversification):
+                          and solution2.fitness_diversification >= solution1.fitness_diversification):
                         solution1.dominated_by.append(solution2)
                         solution1.dominated_by_count += 1
                         is_dominated = True
@@ -145,7 +247,7 @@ class Population:
 
         self.pareto_fronts.append(pareto_front)
 
-    def assign_rank(self):
+    def __assign_rank(self):
         """
             Atribui um rank a cada solução com base em sua dominância em relação às outras soluções.
         """
@@ -164,8 +266,10 @@ class Population:
 
             rank += 1
             current_front = next_front
+            if len(next_front) > 0:
+                self.pareto_fronts.append(next_front)
 
-    def calculate_crowding_distance(self):
+    def __calculate_crowding_distance(self):
         for solution in self.solutions:
             solution.crowding_distance = 0
 
@@ -180,20 +284,22 @@ class Population:
             max_fitness = sorted_solutions[-1].fitness[obj_index]
 
             for i in range(1, len(sorted_solutions) - 1):  # Excluindo o primeiro e último que já são infinitos
-                sorted_solutions[i].crowding_distance += ((sorted_solutions[i + 1].fitness[obj_index] - sorted_solutions[i - 1].fitness[obj_index])
-                                                          / (max_fitness - min_fitness))
+                sorted_solutions[i].crowding_distance += (
+                        (sorted_solutions[i + 1].fitness[obj_index] - sorted_solutions[i - 1].fitness[obj_index])
+                        / (max_fitness - min_fitness))
 
-    def select_survivors(self, num_individuals):
+    def __select_survivors(self):
         survivors = []
 
         for front in self.pareto_fronts:
-            front.sort(key=lambda x: (-x.rank, x.crowding_distance))  # Ordene por rank decrescente e distância de lotação crescente
+            front.sort(key=lambda x: (
+            x.rank, -x.crowding_distance))  # Ordena por rank crescente e crowding distance decrescente
             survivors.extend(front)
 
-            if len(survivors) >= num_individuals:
+            if len(survivors) >= self.num_individuals:
                 break
 
-        return survivors[:num_individuals]  # Retorna os melhores indivíduos para a próxima geração
+        return survivors[:self.num_individuals]  # Retorna os melhores indivíduos para a próxima geração
 
     def get_fitness_profit(self):
         return [individual.fitness_max_profit for individual in self.solutions]
@@ -201,35 +307,46 @@ class Population:
     def get_fitness_diversification(self):
         return [individual.fitness_diversification for individual in self.solutions]
 
+    def __commit_history_solutions(self):
+        profits = [i.fitness_normalized[0] for i in self.solutions]
+        dispersations = [i.fitness_normalized[1] for i in self.solutions]
+        self.solutions_history.append((profits, dispersations))
 
-class Solution:
-    def __init__(self, fitness_max_profit, fitness_diversification, fitness):
-        """
-            Inicializa uma solução individual no contexto do algoritmo NSGA-II.
 
-        Args:
-            fitness_max_profit (float): O valor de fitness para a maximização do lucro máximo.
-            fitness_diversification (float): O valor de fitness para a diversificação.
-            fitness
-        """
-        self.fitness_max_profit = fitness_max_profit
-        self.fitness_diversification = fitness_diversification
-        self.fitness = fitness # lista com todas os fitness
-        self.dominated_by = []  # Lista de soluções que dominam esta solução
-        self.dominated_solutions = []  # Lista de soluções que esta solução domina
-        self.dominated_by_count = 0  # Por quantas soluções esta solução é dominada
-        self.rank = None  # Rank da solução nas frentes de Pareto
-        self.crowding_distance = None  # Distância de lotação da solução
+# class Solution:
+#     def __init__(self, fitness_max_profit, fitness_diversification, fitness):
+#         """
+#             Inicializa uma solução individual no contexto do algoritmo NSGA-II.
+#
+#         Args:
+#             fitness_max_profit (float): O valor de fitness para a maximização do lucro máximo.
+#             fitness_diversification (float): O valor de fitness para a diversificação.
+#             fitness
+#         """
+#         self.fitness_max_profit = fitness_max_profit
+#         self.fitness_diversification = fitness_diversification
+#         self.fitness = fitness # lista com todas os fitness
+#         self.fitness_normalized = 0 # lista com todas os fitness
+#         self.dominated_by = []  # Lista de soluções que dominam esta solução
+#         self.dominated_solutions = []  # Lista de soluções que esta solução domina
+#         self.dominated_by_count = 0  # Por quantas soluções esta solução é dominada
+#         self.rank = None  # Rank da solução nas frentes de Pareto
+#         self.crowding_distance = None  # Distância de lotação da solução
 
 
 # fo
 if __name__ == '__main__':
     arbitrages = []
-    arbitrages.append(Arbitrage("a86e2d9332c3ae6995a042ed4b95fe2b", 59664,	59631, "UNIBET_EU", "ONEXBET",	2.0,	2.02, 0.49))
-    arbitrages.append(Arbitrage("a86e2d9332c3ae6995a042ed4b95fe2b", 59664,	59655, "UNIBET_EU", "PINNACLE",	2.0,	2.03, 0.73))
-    arbitrages.append(Arbitrage("45c50eab0a379bd74d9ad5879ee50595", 94745,	94783, "ONEXBET", "UNIBET_EU",	1.9,	2.12, 0.19))
-    arbitrages.append(Arbitrage("45c50eab0a379bd74d9ad5879ee50595", 94745,	94791, "ONEXBET", "MATCHBOOK",	1.9,	2.14, 0.63))
-    arbitrages.append(Arbitrage("ff03cc32e860ac5cd4c907381443986f", 94795,	94833, "ONEXBET", "UNIBET_EU",	2.12,	1.91, 0.47))
+    arbitrages.append(
+        Arbitrage("a86e2d9332c3ae6995a042ed4b95fe2b", 59664, 59631, "UNIBET_EU", "ONEXBET", 2.0, 2.02, 0.49))
+    arbitrages.append(
+        Arbitrage("a86e2d9332c3ae6995a042ed4b95fe2b", 59664, 59655, "UNIBET_EU", "PINNACLE", 2.0, 2.03, 0.73))
+    arbitrages.append(
+        Arbitrage("45c50eab0a379bd74d9ad5879ee50595", 94745, 94783, "ONEXBET", "UNIBET_EU", 1.9, 2.12, 0.19))
+    arbitrages.append(
+        Arbitrage("45c50eab0a379bd74d9ad5879ee50595", 94745, 94791, "ONEXBET", "MATCHBOOK", 1.9, 2.14, 0.63))
+    arbitrages.append(
+        Arbitrage("ff03cc32e860ac5cd4c907381443986f", 94795, 94833, "ONEXBET", "UNIBET_EU", 2.12, 1.91, 0.47))
     # arbitrages.append(Arbitrage("ff03cc32e860ac5cd4c907381443986f", 94840,	94833, "MATCHBOOK", "UNIBET_EU",	2.12,	1.91, 0.47))
     # arbitrages.append(Arbitrage("6454621f739da2ded2aaae7694e8835a", 50050,	50043, "ONEXBET", "NORDICBET",	2.08,	1.95, 0.64))
     # arbitrages.append(Arbitrage("6454621f739da2ded2aaae7694e8835a", 50771,	50764, "ONEXBET", "NORDICBET",	2.08,	1.95, 0.64))
@@ -241,16 +358,4 @@ if __name__ == '__main__':
 
     # individuo = Individual(arbitrages, 100)
     population = Population(5, arbitrages, 100)
-    print(population.pareto_fronts)
-
-
-
-
-
-
-
-
-
-
-
-
+    population.main(5)
