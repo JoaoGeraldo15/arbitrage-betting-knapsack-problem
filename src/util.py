3b9926503fd81df58e3c9289dfe66a3d89d6bff2
joaogeraldo15 <joaogeraldo40@gmail.com>
2023-11-11 19:17:25 -0300

    Feat: Método para identificar a melhor geração com soluções viáveis.

--- src/util.py
+++ src/util.py
@@ -1,6 +1,7 @@
 import math
 import os
 import datetime
+import pickle
 import uuid
 from enum import Enum
 
@@ -36,35 +37,34 @@ def replace_api_key(api_key):
             file.write(line)
 
 
-class BookmakerWithdrawEnum(Enum):
-    # Saques mínimo via PIX
-    BETSSON = 50
-    MATCHBOOK = 10  # https://www.matchbook.com/page/faqs/accountfunding
-    PINNACLE = 30  #  https://www.pinnacle.com/pt/future/legal-payments
-    WILLIAMHILL = 10  #  https://williamhill-lang.custhelp.com/app/answers/detail/a_id/28879
-    SPORT_888 = 50  #  https://www.888sport.com/pt/banking/withdrawal/policy/#CashOutPolicy
-    ONEXBET = 30  #  https://br.1xbet.com/information/payment
-    UNIBET_EU = 10
-    BETONLINEAG = 50
-    NORDICBET = 20
+class PickleSerializer:
+    @staticmethod
+    def save(obj, filename):
+        """Salva o objeto em um arquivo usando pickle."""
+        with open(filename, 'wb') as file:
+            pickle.dump(obj, file)
+
+    @staticmethod
+    def load(filename):
+        """Carrega um objeto de um arquivo usando pickle."""
+        with open(filename, 'rb') as file:
+            return pickle.load(file)
 
 
 class ExportadorDeGraficos:
 
     def __init__(self):
         self.data_hora_atual = f'{datetime.datetime.now().strftime("%Y_%m_%d_[%H:%M:%S]")}_{str(uuid.uuid4())[:5]}'
-        print(self.data_hora_atual)
         os.mkdir(self.data_hora_atual)
 
     def get_file_name(self, geracoes, extension=''):
         return f'{self.data_hora_atual}_[G:{geracoes}].{extension}'
 
-
     def plot(self, x, y, generation):
         plt = matplotlib.pyplot
         plt.figure()
-        plt.xlim(0, 1.05)
-        plt.ylim(0, 1.05)
+        # plt.xlim(min(x), 1.05)
+        # plt.ylim(min(y), 1.05)
         plt.title(f'{generation}° Generation')
         plt.xlabel('Profit')
         plt.ylabel('Dispersation')
@@ -84,7 +84,7 @@ class ExportadorDeGraficos:
         plt.ylim(0, 1.05)
         plt.xlabel('Profit')
         plt.ylabel('Dispersation')
-        file_name = self.data_hora_atual+ '/' + self.get_file_name(population.n_generations, 'gif')
+        file_name = self.data_hora_atual + '/' + self.get_file_name(population.n_generations, 'gif')
 
         with writer.saving(fig, file_name, 100):
             for i in range(len(population.solutions_history)):
@@ -96,30 +96,45 @@ class ExportadorDeGraficos:
         plt.close()
 
     def __plot_hv(self, x, y, y_axis_limit, title, filename):
+
         plt = matplotlib.pyplot
         plt.xlabel('Generation')
-        plt.ylabel('Hypervolume')
+        plt.ylabel('Hypervolume Score')
         plt.title(title)
         plt.xlim(-1, max(x) + 2)
         plt.ylim(0, y_axis_limit)
-        plt.plot(x, y)
-        plt.grid(True)
+        plt.scatter(x, y, s=15)
         plt.savefig(self.data_hora_atual + '/' + filename)
         plt.close()
 
-    def hypervolume_plot(self, population):
-        reference_point = [1.1, 1.1]
+    @staticmethod
+    def set_best_frontier(population):
+        FITNESS_INDEX = 1
+        reference_point = [1.05, 1.05]
         x_values = [x for x, _ in population.pareto_history_front_normalized]
-        y_values = [pg.hypervolume([i for i in y]).compute(reference_point) for _, y in population.pareto_history_front_normalized]
+        y_values = [1 / pg.hypervolume([i for i in y]).compute(reference_point) for _, y in
+                    population.pareto_history_front_normalized]
 
-        y_values_reversed = [1 / pg.hypervolume([i for i in y]).compute(reference_point) for _, y in population.pareto_history_front_normalized]
-        max_value = max(y_values_reversed)
-        y_values_reversed = [i / max_value for i in y_values_reversed]
-        title = f'[P]: {population.n_individuals} [G]: {population.n_generations} [M]: {population.mutation_rate} [C]: {population.crossover_strategy}'
-        self.__plot_hv(x_values, y_values_reversed, max(y_values_reversed) + max(y_values_reversed) * 0.1, title, 'hypervolume.png')
+        aux = y_values.copy()
+        found = False
+        index = None
+        while found is False and len(aux) != 0:
+            index = aux.index(max(aux))
+            if len(set(population.pareto_history_front_normalized[index][FITNESS_INDEX])) < 5:
+                aux.pop(index)
+                continue
+            found = True
 
-        y_values_reversed = [math.log10(i + 1) for i in y_values_reversed]
-        self.__plot_hv(x_values, y_values_reversed, max(y_values_reversed) + max(y_values_reversed) * 0.1, title + ' log10', 'hypervolume_log.png')
+        population.best_frontier = index  # Salvando a fronteira que teve o maior hypervolume e é viável
+
+    def hypervolume_plot(self, population):
+        reference_point = [1.05, 1.05]
+        x_values = [x for x, _ in population.pareto_history_front_normalized]
+        y_values = [1 / pg.hypervolume([i for i in y]).compute(reference_point) for _, y in
+                             population.pareto_history_front_normalized]
+
+        title = f'Population: {population.n_individuals} Mutation: {population.mutation_rate}'
+        self.__plot_hv(x_values, y_values, max(y_values) * 1.1, title, 'hypervolume.png')
 
     def __config_sub_plot_hv(self) -> matplotlib.pyplot:
         plot_config = matplotlib.pyplot
@@ -129,42 +144,42 @@ class ExportadorDeGraficos:
         return plot_config
 
     def hypervolume_plots(self, population_list: [], main_title: str):
-        reference_point = [1.1, 1.1]
-        x_values = [x for x, _ in population_list[0].pareto_history_front_normalized]
+        reference_point = [1.05, 1.05]
 
-        n_columns = 3
+        n_columns = 4
         n_rows = int(np.ceil(len(population_list) / n_columns))
 
         plt.figure(figsize=(n_columns * 4, n_rows * 4))
         count = 1
 
         for population in population_list:
+            x_values = [x for x, _ in population.pareto_history_front_normalized]
             y_values_reversed = [
                 1 / pg.hypervolume([i for i in y]).compute(reference_point) for
                 _, y in population.pareto_history_front_normalized]
 
-            max_value = max(y_values_reversed)
-            y_values_reversed = [i / max_value for i in y_values_reversed]
+            # max_value = max(y_values_reversed)
+            # y_values_reversed = [i / max_value for i in y_values_reversed]
             # y_values_reversed = [math.log10(i + 1) for i in y_values_reversed]
-            # title = f'Population: {population.n_individuals} Generations: {population.n_generations} Mutation: {population.mutation_rate}'
-            title = f'[P]: {population.n_individuals} [G]: {population.n_generations} [M]: {population.mutation_rate}'
+            title = f'[P]: {population.n_individuals} [G]: {population.n_generations} [M]: {population.mutation_rate} [C]: {population.crossover_rate}'
 
             plt.subplot(n_rows, n_columns, count)
-            plt.plot(x_values, y_values_reversed)
+            # plt.plot(x_values, y_values_reversed)
+            plt.scatter(x_values, y_values_reversed)
             plt.title(title)
             count += 1
 
         plt.suptitle(main_title, fontsize=16)  # Título principal
         # plt.tight_layout() # Ajuste o layout dos subgráficos
         plt.subplots_adjust(wspace=0.2, hspace=0.5)
-        plt.savefig(f'{self.data_hora_atual}.png')
+        plt.savefig(f'{self.data_hora_atual}_{main_title}_.pdf')
         plt.close()
 
-        imagem = Image.open(f'{self.data_hora_atual}.png')
-        imagem.show()
+        # imagem = Image.open(f'{self.data_hora_atual}.png')
+        # imagem.show()
 
     def grid_hypervolume(self, population_list: [], params):
-        reference_point = [1.1, 1.1]
+        reference_point = [1.05, 1.05]
 
         # n_rows = int(np.ceil(np.sqrt(len(population_list))))
         # n_columns = int(np.ceil(len(population_list) / n_rows))
@@ -194,7 +209,8 @@ class ExportadorDeGraficos:
             plt.plot(x_values, y_values_reversed)
             plt.title(title)
 
-            if population.n_individuals == params[0] and population.n_generations == params[1] and population.mutation_rate == params[2] and population.crossover_strategy == params[3]:
+            if population.n_individuals == params[0] and population.n_generations == params[
+                1] and population.mutation_rate == params[2] and population.crossover_strategy == params[3]:
                 plt.title(title, color='green')
 
             count += 1
@@ -206,3 +222,12 @@ class ExportadorDeGraficos:
 
         # imagem = Image.open(f'{self.data_hora_atual}.pdf')
         # imagem.show()
+
+    def get_max_hypervolume(self, population):
+        reference_point = [1.05, 1.05]
+        x_values = [x for x, _ in population.pareto_history_front_normalized]
+        y_values_reversed = [
+            1 / pg.hypervolume([i for i in y]).compute(reference_point) for
+            _, y in population.pareto_history_front_normalized]
+
+        return max(y_values_reversed)
